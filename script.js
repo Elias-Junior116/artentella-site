b127c59510a027212f7f431239c33406f36c1773
Elias-Junior116 <leocdoc@gmail.com>
2025-05-10 01:09:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
 
-let current = 0;
-const images = document.querySelectorAll('.carousel img');
-setInterval(() => {
-    images[current].classList.remove('active');
-    current = (current + 1) % images.length;
-    images[current].classList.add('active');
-}, 3000);
